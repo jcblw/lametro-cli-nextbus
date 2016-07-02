97e16dcd0d690f4f86a443deb2118f3aa8b447b3
jcblw <jacoblowe2.0@gmail.com>
2016-07-02 14:37:34 -0700

    adding updates

---
+++ README.md
@@ -0,0 +1,3 @@
+# Making CLI tool
+
+This is an example on how to make a cli tool with node.js and npm.
--- api.js
+++ api.js
@@ -2,7 +2,10 @@
 const got = require('got')
 
 function getNextTime (callback) {
-  got.get('http://api.metro.net/agencies/lametro/stops/29/predictions/', {json: true})
+  got.get(
+    'http://api.metro.net/agencies/lametro/stops/29/predictions/',
+    {json: true}
+  )
     .then(res => callback(null, res.body.items[0]))
     .catch(callback)
 }
